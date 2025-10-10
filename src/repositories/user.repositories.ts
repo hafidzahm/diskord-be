712f6c19da072b4b65edf6db24e45a31a981e149
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-11 06:27:56 +0700

    chore: function createUser on UserRepositories

--- src/repositories/user.repositories.ts
+++ src/repositories/user.repositories.ts
@@ -1,10 +1,33 @@
+import type { RoleType } from "@prisma/client";
 import prisma from "../utils/prisma.js";
+import type { SignUpSchemaType } from "../utils/schema/user.schema.js";
 
 class UserRepositories {
   static async isEmailExist(email: string) {
     return await prisma.user.count({
       where: {
         email: email,
+      },
+    });
+  }
+
+  static async findRole(role: RoleType) {
+    return prisma.role.findFirstOrThrow({
+      where: {
+        role: role,
+      },
+    });
+  }
+
+  static async createUser(data: SignUpSchemaType, photo: string) {
+    const role = await this.findRole("USER");
+    return prisma.user.create({
+      data: {
+        email: data.email,
+        name: data.name,
+        password: data.password,
+        photo: photo,
+        RoleId: role.id,
       },
     });
   }
