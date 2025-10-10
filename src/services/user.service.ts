d1b53d0372f3562745502ba5a494fb0aa79f5268
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-11 06:37:51 +0700

    chore: implement createUser and bcrypt

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,6 +1,7 @@
 import "multer"; // This loads the global types
 import type { SignUpSchemaType } from "../utils/schema/user.schema.js";
 import UserRepositories from "../repositories/user.repositories.js";
+import bcrypt from "bcrypt";
 
 class UserService {
   static async signUp(data: SignUpSchemaType, file: Express.Multer.File) {
@@ -8,6 +9,17 @@ class UserService {
     if (isEmailExist > 1) {
       throw new Error("Email must be unique");
     }
+
+    const user = await UserRepositories.createUser(
+      {
+        ...data,
+        password: bcrypt.hashSync(
+          data.password,
+          process.env.SECRET_KEY_BCRYPT as string
+        ),
+      },
+      file.filename
+    );
   }
 }
 
