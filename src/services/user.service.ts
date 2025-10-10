16b1eeb4f4e40102d5af56509839d43fa988e00d
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-11 06:16:44 +0700

    chore: implement isEmailExist on UserService

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,8 +1,14 @@
 import "multer"; // This loads the global types
 import type { SignUpSchemaType } from "../utils/schema/user.schema.js";
+import UserRepositories from "../repositories/user.repositories.js";
 
 class UserService {
-  static signUp(data: SignUpSchemaType, file: Express.Multer.File) {}
+  static async signUp(data: SignUpSchemaType, file: Express.Multer.File) {
+    const isEmailExist = await UserRepositories.isEmailExist(data.email);
+    if (isEmailExist > 1) {
+      throw new Error("Email must be unique");
+    }
+  }
 }
 
 export default UserService;
