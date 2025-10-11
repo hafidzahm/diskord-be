fcf93334a90e0e023a192d02a6aa02c9baf6975a
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-11 14:58:49 +0700

    implementing static public, cors, and body-parser

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 const express = require("express");
 import type { Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
+
+app.use(express.static("public")); //public static folder
+app.use(cors()); //cors
+app.use(bodyParser.json()); //bodyparser for json
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
