ff1c6452b42b1b5d21c75e50ebe3de52631906f0
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-11 14:54:00 +0700

    implementing multer on signup route

---
+++ src/routes/user.routes.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import multer from "multer";
+import { storageUserPhoto } from "../utils/multer.js";
+import UserController from "../controllers/user.controller.js";
+const userRoutes = express.Router();
+
+const uploadPhoto = multer({
+  storage: storageUserPhoto,
+  fileFilter(req, file, callback) {
+    if (file.mimetype.startsWith("image/")) {
+      callback(null, true);
+    }
+    callback(null, false);
+  },
+});
+
+userRoutes.post(
+  "/auth/sign-up",
+  uploadPhoto.single("photo"),
+  UserController.signUp
+);
