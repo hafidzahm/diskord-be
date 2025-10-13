0e19f6529660494bdca0105a009828cd4ee8b1bc
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-13 11:25:16 +0700

    implementing controller

--- src/controllers/group.controller.ts
+++ src/controllers/group.controller.ts
@@ -1,10 +1,49 @@
-import type { Request, Response, NextFunction } from "express";
+import type { Response, NextFunction } from "express";
+import GroupService from "../services/group.service.ts";
+import type { CustomRequest } from "../types/custom.request.type.ts";
+import { createFreeGroupSchema } from "../utils/schema/group.schema.ts";
 
 class GroupController {
   static async createFreeGroup(
-    req: Request,
+    req: CustomRequest,
     res: Response,
     next: NextFunction
-  ) {}
+  ) {
+    const validatedData = createFreeGroupSchema.safeParse(req.body);
+    if (!validatedData.success) {
+      const errorMessages = validatedData.error.issues.map(
+        (err) => `${err.path} - ${err.message}`
+        // name - name must string
+      );
+
+      throw {
+        type: "ZodValidationError",
+        success: false,
+        message: "Validation error",
+        details: errorMessages,
+      };
+    }
+
+    // validasi req.file
+    if (!req.file) {
+      throw {
+        type: "BadRequest",
+        success: false,
+        message: "Image file required",
+      };
+    }
+
+    const group = await GroupService.createFreeGroup(
+      validatedData.data,
+      req.file.filename,
+      req?.user?.id as string
+    );
+
+    return res.status(201).json({
+      success: true,
+      message: "Free group created",
+      data: group,
+    });
+  }
 }
 export default GroupController;
--- src/utils/schema/group.schema.ts
+++ src/utils/schema/group.schema.ts
@@ -1,6 +1,6 @@
 import z from "zod";
 
-const createFreeGroupSchema = z.object({
+export const createFreeGroupSchema = z.object({
   name: z.string().min(3),
   about: z.string(),
 });
