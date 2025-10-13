559a71ce9366b5a8934b136871604977c735967c
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-13 10:54:59 +0700

    add instanceof JsonWebTokenError error handling

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,4 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
 
 export default function AuthMiddleware(
   req: Request,
@@ -13,5 +14,23 @@ export default function AuthMiddleware(
       message: "Invalid token",
     };
   }
+
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer") {
+    throw {
+      type: "AuthenticationError",
+      success: false,
+      message: "Invalid token",
+    };
+  }
+
+  const decoded = jwt.verify(
+    token as string,
+    process.env.SECRET_AUTH as string
+  );
+
+  console.log(decoded, "<---- decoded");
+
   next();
 }
