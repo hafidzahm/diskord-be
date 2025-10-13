677c96ad8f1ff9a8751ba88e097a59b88c805877
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-13 14:50:38 +0700

    refactor storageMulter

--- src/routes/group.routes.ts
+++ src/routes/group.routes.ts
@@ -2,12 +2,28 @@ import express from "express";
 import GroupController from "../controllers/group.controller.ts";
 import AuthMiddleware from "../middlewares/auth.middleware.ts";
 import multer from "multer";
-import { storageGroupPhoto } from "../utils/multer.ts";
+import {
+  storageFreeGroupPhoto,
+  storagePaidGroupPhoto,
+} from "../utils/multer.ts";
 const groupRoutes = express.Router();
 
-const uploadGroupPhoto = multer({
-  storage: storageGroupPhoto,
+const uploadFreeGroupPhoto = multer({
+  storage: storageFreeGroupPhoto,
   fileFilter(req, file, callback) {
+    if (file.mimetype.startsWith("image/")) {
+      callback(null, true);
+    }
+    callback(null, false);
+  },
+});
+
+const uploadPaidGroupPhoto = multer({
+  storage: storagePaidGroupPhoto,
+  fileFilter(req, file, callback) {
+    if (file.filename === "assets") {
+      callback(null, true);
+    }
     if (file.mimetype.startsWith("image/")) {
       callback(null, true);
     }
@@ -18,8 +34,18 @@ const uploadGroupPhoto = multer({
 groupRoutes.post(
   "/group/free",
   AuthMiddleware,
-  uploadGroupPhoto.single("photo"),
+  uploadFreeGroupPhoto.single("photo"),
   GroupController.createFreeGroup
+);
+
+groupRoutes.post(
+  "/group/paid",
+  AuthMiddleware,
+  uploadPaidGroupPhoto.fields([
+    { name: "photo", maxCount: 1 },
+    { name: "assets" },
+  ]),
+  GroupController.createPaidGroup
 );
 
 export default groupRoutes;
