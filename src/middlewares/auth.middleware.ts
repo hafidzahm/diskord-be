382f2d36672181aa24f35338dc141ef519d09dbb
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-13 10:55:49 +0700

    implement auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
-
+//AuthMiddleware
 export default function AuthMiddleware(
   req: Request,
   res: Response,
