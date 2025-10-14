5690fb43229cc36c0eeab9f26cbb8b464ad77bea
Abdul Hafizh <hafizhahm123@gmail.com>
2025-10-14 13:40:00 +0700

    extending update free schema

--- src/utils/schema/group.schema.ts
+++ src/utils/schema/group.schema.ts
@@ -10,5 +10,8 @@ export const createPaidGroupSchema = createFreeGroupSchema.extend({
   benefit: z.array(z.string()).min(1),
 });
 
+export const updateFreeGroupSchema = createFreeGroupSchema;
+
 export type CreateGroupSchemaType = z.infer<typeof createFreeGroupSchema>;
 export type CreatePaidGroupSchemaType = z.infer<typeof createPaidGroupSchema>;
+export type UpdateFreeGroupSchemaType = CreateGroupSchemaType;
